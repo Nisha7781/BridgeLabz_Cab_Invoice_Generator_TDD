dd857d44ab9af1b9bb8694b1eb5c377393fea592
Nisha7781 <nishamali0781@gmail.com>
2024-04-16 17:47:52 +0530

    Written code to pass testcase to check it return minimumFare if distance and time given is less

--- src/main/java/org/example/CabInvoiceService.java
+++ src/main/java/org/example/CabInvoiceService.java
@@ -4,9 +4,18 @@ public class CabInvoiceService
 {
     private static final double CostPerKM = 10;
     private static final int RatePerMin = 1;
+    private static final int MinimumFare = 5;
     public double calculateFare(double distance, int time)
     {
-        return distance * CostPerKM + time * RatePerMin;
+        double TotalFare =  distance * CostPerKM + time * RatePerMin;
+        if(TotalFare < MinimumFare)
+        {
+            return MinimumFare;
+        }
+        else
+        {
+            return TotalFare;
+        }
     }
 
 }
