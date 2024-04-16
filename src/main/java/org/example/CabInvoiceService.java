bbfd426004c2a049cb5df658110fefbebdd3a5e5
Nisha7781 <nishamali0781@gmail.com>
2024-04-16 17:38:12 +0530

    Written code to pass test case to check total fare if distance and time given

--- src/main/java/org/example/CabInvoiceService.java
+++ src/main/java/org/example/CabInvoiceService.java
@@ -2,10 +2,11 @@ package org.example;
 
 public class CabInvoiceService
 {
-
+    private static final double CostPerKM = 10;
+    private static final int RatePerMin = 1;
     public double calculateFare(double distance, int time)
     {
-        return 0;
+        return distance * CostPerKM + time * RatePerMin;
     }
 
 }
